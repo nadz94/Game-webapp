07a8df6faa5d3f519c72471e61dfc8162e81f3b6
StrandemEnterprise <strandem.enterprise@gmail.com>
2026-01-01 20:21:28 +0000

    Update Firebase configuration to pixel-hajj project

--- js/firebase.js
+++ js/firebase.js
@@ -4,13 +4,13 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.1/firebase
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyB1qbLxq_XVfMDOeOetBvyAsM4ePwao7rQ",
-    authDomain: "bit-hajj.firebaseapp.com",
-    projectId: "bit-hajj",
-    storageBucket: "bit-hajj.firebasestorage.app",
-    messagingSenderId: "350092156846",
-    appId: "1:350092156846:web:811453f1d792bf99c9f23f",
-    measurementId: "G-B8T46E3G65"
+    apiKey: "AIzaSyCIcnPtUiutzf7dfy1kKaDg06CuaZBgnoI",
+    authDomain: "pixel-hajj.firebaseapp.com",
+    projectId: "pixel-hajj",
+    storageBucket: "pixel-hajj.firebasestorage.app",
+    messagingSenderId: "646555868440",
+    appId: "1:646555868440:web:e039b371bdb27dac07926d",
+    measurementId: "G-6F72QMYCTZ"
 };
 
 // Initialize Firebase
